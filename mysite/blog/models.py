a369604d0ffe8ffe547af7e6edbd3fa46551cb06
Astryda Malinowska <astamalinowska@outlook.com>
2023-09-18 22:03:00 +0200

    Updated Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-
+# Post model
 class Post(models.Model):
     title = models.CharFiels(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
