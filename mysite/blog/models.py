90879710603627ec8a94078da9d9507d746364e0
Astryda Malinowska <astamalinowska@outlook.com>
2023-09-18 23:22:38 +0200

    Created custom publish manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+# Craeting own publish manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.Published)
+
+
 # Post model
 class Post(models.Model):
 
@@ -19,6 +26,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    object = models.Manager()
+    published = PublishedManager()
 
 
     class Meta:
