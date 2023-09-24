2e0c763fcb95c7dcdef2524c8eaca36179852255
Astryda Malinowska <astamalinowska@outlook.com>
2023-09-24 23:00:25 +0200

    Added views for post list and post detail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'post': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("Nie znaleziono posta.")
+
+    return render(request, 'blog/post/detail.html', {'post': post})
